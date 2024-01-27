9dc3a9efd2073f7244ef0d812271f8cdb45858d6
Park Ji-Ho <jiho010920@gmail.com>
2024-01-27 14:54:51 +0900

    baekjoon-1912(ing)

--- baekjoon/bj-1912(ing).py
+++ baekjoon/bj-1912(ing).py
@@ -1,6 +1,20 @@
-# 연속합
+# 연속합 DP로 해결 필요
+import sys
 
-n = int(input())
+n = int(sys.stdin.readline())
 
-num = list(map(int, input().split()))
+num = list(map(int, sys.stdin.readline().split()))
 
+answer = -1000
+
+for i in range(n):
+  tmp = 0
+  for j in range(i, n):
+    tmp += num[j]
+    if (tmp > answer):
+      answer = tmp
+    elif (tmp < 0):
+      break
+    
+
+print(answer)
