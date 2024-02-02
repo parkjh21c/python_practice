d268a15741e2724dd2b0e0f7d88fffcace4b19df
Park Ji-Ho <jiho010920@gmail.com>
2024-02-02 18:04:45 +0900

    3개 숫자 이상 덧셈 가능

--- Calculator_project/main.py
+++ Calculator_project/main.py
@@ -24,41 +24,47 @@ while (1):
 
   # 수식을 리스트로 변환
   calcul_formula_list = calcul_formula.split()
+  calcul_formula_lest_len = len(calcul_formula_list)
 
   # 계산
   calcul = cal.calculate()
 
   try:
-    calcul_formula_list[0] = calcul.change_type(calcul_formula_list[0])
-    calcul_formula_list[2] = calcul.change_type(calcul_formula_list[2])
+    for i in range(0, calcul_formula_lest_len, 2):
+      calcul_formula_list[i] = calcul.change_type(calcul_formula_list[i])
+
   except:
-    print("옳바르지 않은 수식입니다!")
+    print("옳바르지 않은 수식입니다! (오류코드 0)")
     print()
     print("************************************")
     time.sleep(1)
     continue
-
-  if (calcul_formula_list[1] == "+"):
-    answer = calcul.add(calcul_formula_list[0], calcul_formula_list[2])
-
-  elif (calcul_formula_list[1] == "-"):
-    answer = calcul.minus(calcul_formula_list[0], calcul_formula_list[2])
-
-  elif (calcul_formula_list[1] == "*"):
-    answer = calcul.multiplication(calcul_formula_list[0], calcul_formula_list[2])
-
-  elif (calcul_formula_list[1] == "/"):
-    answer = calcul.division(calcul_formula_list[0], calcul_formula_list[2])
   
-  elif (calcul_formula_list[1] == "**"):
-    answer = calcul.square(calcul_formula_list[0], calcul_formula_list[2])
+  answer = calcul_formula_list[0]
 
-  else:
-    print("옳바르지 않은 수식입니다!")
-    print()
-    print("************************************")
-    time.sleep(1)
-    continue
+  # 기호만 검사
+  for i in range(1, calcul_formula_lest_len, 2):
+    if (calcul_formula_list[i] == "+"):
+      answer = calcul.add(answer, calcul_formula_list[i + 1])
+
+    elif (calcul_formula_list[i] == "-"):
+      answer = calcul.minus(answer, calcul_formula_list[i + 1])
+
+    elif (calcul_formula_list[i] == "*"):
+      answer = calcul.multiplication(answer, calcul_formula_list[i + 1])
+
+    elif (calcul_formula_list[i] == "/"):
+      answer = calcul.division(answer, calcul_formula_list[i + 1])
+    
+    elif (calcul_formula_list[i] == "**"):
+      answer = calcul.square(answer, calcul_formula_list[i + 1])
+
+    else:
+      print("옳바르지 않은 수식입니다! (오류코드 1)")
+      print()
+      print("************************************")
+      time.sleep(1)
+      continue
 
   print(f"값 : {answer}")
   print()
