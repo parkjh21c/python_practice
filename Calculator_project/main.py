b9653365a30e78506fab766d2d11d3de6cbf3d71
Park Ji-Ho <jiho010920@gmail.com>
2024-02-03 14:27:08 +0900

    change 1.1

--- Calculator_project/main.py
+++ Calculator_project/main.py
@@ -11,23 +11,83 @@ print()
 print("************************************")
 time.sleep(1)
 
+# 숫자, 기호 순서
+count = 0
+calcul = cal.calculate()
+
 # 입력
 while (1):
   print("식을 입력하세요")
   print("종료하려면 -1을 입력하세요")
-  print("ex) 1 + 2")
 
-  calcul_formula = input("계산 식 : ")
+  # 숫자 입력받기
+  if (count % 2 == 0):
+    num_input = int(input("값 : "))
 
-  if (calcul_formula == "-1"):
-    break
 
-  # 수식을 리스트로 변환
-  calcul_formula_list = calcul_formula.split()
-  calcul_formula_lest_len = len(calcul_formula_list)
+  # 기호 입력받기
+  else:
+    sign = input("값 : ")
+
+  # 여기서부터 수정 필요
+
+  if (sign == "+"):
+    answer = calcul.add(answer, calcul_formula_list[i + 1])
+
+  elif (sign == "-"):
+    answer = calcul.minus(answer, calcul_formula_list[i + 1])
+
+  elif (sign == "*"):
+    answer = calcul.multiplication(answer, calcul_formula_list[i + 1])
+
+  elif (sign == "/"):
+    answer = calcul.division(answer, calcul_formula_list[i + 1])
+  
+  elif (sign == "**"):
+    answer = calcul.square(answer, calcul_formula_list[i + 1])
+
+  else:
+    print("옳바르지 않은 수식입니다! (오류코드 1)")
+    print()
+    print("************************************")
+    time.sleep(1)
+    continue
+
+
+
+  
+
+  # 숫자 or 기호 받을건지 계산
+  count = 0
+
+  if (count % 2 == 1):
+    if (calcul_formula_list[i] == "+"):
+      answer = calcul.add(answer, calcul_formula_list[i + 1])
+
+    elif (calcul_formula_list[i] == "-"):
+      answer = calcul.minus(answer, calcul_formula_list[i + 1])
+
+    elif (calcul_formula_list[i] == "*"):
+      answer = calcul.multiplication(answer, calcul_formula_list[i + 1])
+
+    elif (calcul_formula_list[i] == "/"):
+      answer = calcul.division(answer, calcul_formula_list[i + 1])
+    
+    elif (calcul_formula_list[i] == "**"):
+      answer = calcul.square(answer, calcul_formula_list[i + 1])
+
+    else:
+      print("옳바르지 않은 수식입니다! (오류코드 1)")
+      print()
+      print("************************************")
+      time.sleep(1)
+      continue
+
+  
+
 
   # 계산
-  calcul = cal.calculate()
+
 
   try:
     for i in range(0, calcul_formula_lest_len, 2):
@@ -70,3 +130,4 @@ while (1):
   print()
   print("************************************")
   time.sleep(1)
+  
