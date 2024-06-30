a95402f492a993f11efa4669b698be1a45db4c47
parkjh21c <jiho010920@gmail.com>
2024-06-30 20:41:49 +0900

    .

---
+++ baekjoon/bj-1920.py
@@ -0,0 +1,23 @@
+# 트리 구현
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class makeTree:
+    def __init__(self):
+        pass
+
+    def addNode(self, Node):
+        pass
+
+import sys
+
+N = int(sys.stdin.readline())
+
+mainTree = makeTree()
+
+for i in range(N):
+    d = int(sys.stdin.readline())
+
