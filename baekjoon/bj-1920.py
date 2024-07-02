b4360b5a0cb242b0df9d5035e1b39951b921ecea
Park Ji-Ho <jiho010920@gmail.com>
2024-07-02 16:49:56 +0900

    ing..

--- baekjoon/bj-1920.py
+++ baekjoon/bj-1920.py
@@ -5,19 +5,62 @@ class Node:
         self.left = None
         self.right = None
 
-class makeTree:
+class Tree:
     def __init__(self):
-        pass
+        self.root = None
 
-    def addNode(self, Node):
-        pass
+def addNode(self , node):
+    while (1):
+        if (self.root == None):
+            self.root = node
+            break
+        elif (node.data > self.root.data):
+            self.root = self.root.right
+        elif (node.data < self.root.data):
+            self.root = self.root.left
+        else:
+            break
+
+def findNode(self, data):
+    while (1):
+        if (self.root == None):
+            print("0")
+            break
+        elif (data > self.root.data):
+            self.root = self.root.right
+        elif (data < self.root.data):
+            self.root = self.root.left
+        else:
+            print("1")
+            break
+        
+def postorder(self, n):
+    if n != None:
+        if n.left:
+            self.postorder(n.left)
+        if n.right:
+            self.postorder(n.right)
+        print(n.data, '', end='')
 
 import sys
 
 N = int(sys.stdin.readline())
 
-mainTree = makeTree()
+tree = Tree()
 
-for i in range(N):
-    d = int(sys.stdin.readline())
+n_list = list(map(int, sys.stdin.readline().split()))
+
+for i in n_list:
+    node = Node(i)
+    addNode(tree ,node)
+
+postorder(tree)
+
+M = int(sys.stdin.readline())
+
+m_list = list(map(int, sys.stdin.readline().split()))
+
+for j in m_list:
+    findNode(tree, j)
+
 
