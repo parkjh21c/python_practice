5c173f24953570059229532dbf262d10cd5c4a96
Park Ji-Ho <jiho010920@gmail.com>
2024-07-03 16:21:50 +0900

    ing

--- baekjoon/bj-1920.py
+++ baekjoon/bj-1920.py
@@ -9,38 +9,39 @@ class Tree:
     def __init__(self):
         self.root = None
 
-def addNode(self , node):
-    while (1):
-        if (self.root == None):
-            self.root = node
-            break
-        elif (node.data > self.root.data):
-            self.root = self.root.right
-        elif (node.data < self.root.data):
-            self.root = self.root.left
+    def addNode(self , data):
+        if self.root is None:
+            self.root = Node(data)
         else:
-            break
+            p = self.root
+            while (1):
+                if data < p.data:
+                    p = p.left
+                elif (data > p.data):
+                    p = p.right
+                else:
+                    break
 
-def findNode(self, data):
-    while (1):
-        if (self.root == None):
-            print("0")
-            break
-        elif (data > self.root.data):
-            self.root = self.root.right
-        elif (data < self.root.data):
-            self.root = self.root.left
-        else:
-            print("1")
-            break
+    def findNode(self, data):
+        while (1):
+            if (self.root == None):
+                print("0")
+                return
+            elif (data > self.root.data):
+                self.root = self.root.right
+            elif (data < self.root.data):
+                self.root = self.root.left
+            else:
+                print("1")
+                return
         
-def postorder(self, n):
-    if n != None:
-        if n.left:
-            self.postorder(n.left)
-        if n.right:
-            self.postorder(n.right)
-        print(n.data, '', end='')
+    def postorder(self, n):
+        if n != None:
+            if n.left:
+                self.postorder(n.left)
+            if n.right:
+                self.postorder(n.right)
+            print(n.data, '', end='')
 
 import sys
 
@@ -51,16 +52,15 @@ tree = Tree()
 n_list = list(map(int, sys.stdin.readline().split()))
 
 for i in n_list:
-    node = Node(i)
-    addNode(tree ,node)
+    tree.addNode(i)
 
-postorder(tree)
+tree.postorder(tree.root)
 
 M = int(sys.stdin.readline())
 
 m_list = list(map(int, sys.stdin.readline().split()))
 
 for j in m_list:
-    findNode(tree, j)
+    tree.findNode(j)
 
 
